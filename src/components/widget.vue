b15885ebe28d94fb787c7f08aed57c996531796f
Azamat Kagarman Home <azamat.kagarman@gmail.com>
2023-02-05 18:15:58 +0600

    weatherAPI

--- src/components/widget.vue
+++ src/components/widget.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="widget-wrapper">
-      <div v-if="!settings">
+      <div v-if="!settings && componentLoaded">
          <header class="d-flex flex-row justify-content-between align-items-center mb-2">
             <div class="header-city">
                {{ weatherInfo.name }} {{ weatherInfo.sys?.country }}
@@ -35,14 +35,16 @@
       </div>
       <Settings v-if="settings" @setSettings="setSettings" :cities="cities" :chosenCity="chosenCity" @addCity="addCity"
          @setChosenCity="setChosenCity" @deleteCity="deleteCity" />
+      <Icon v-if="!componentLoaded" icon="eva:loader-outline" width="60" />
    </div>
 </template>
 
 <script>
 import { Icon } from '@iconify/vue';
-import Settings from './settings.vue'
+import Settings from './settings.vue';
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from 'axios'
+import axios from 'axios';
+import { getCoordinates, getWeatherInfo } from "../services/weatherAPI"
 
 export default {
    data() {
@@ -52,6 +54,7 @@ export default {
          chosenCity: '',
          settings: false,
          cities: [],
+         componentLoaded: false
       }
    },
    components: {
@@ -60,37 +63,30 @@ export default {
    },
    methods: {
       async fetchWeatherData() {
-         try {
-            const coordinatesRes = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${this.chosenCity}&limit=5&appid=10ecbab3e9aca4de70736689048eeee4`)
-            const ChosenCityCoordination = coordinatesRes.data[0]
-
-            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ChosenCityCoordination.lat}&lon=${ChosenCityCoordination.lon}&appid=10ecbab3e9aca4de70736689048eeee4&units=metric`)
-            this.weatherInfo = response.data
-            this.weatherMainInfo = response.data.weather[0]
-         } catch (error) {
-            console.log(error)
-         }
-
+         const ChosenCityCoordination = await getCoordinates(this.chosenCity)
+         this.weatherInfo = await getWeatherInfo(ChosenCityCoordination)
+         this.weatherMainInfo = await this.weatherInfo.weather[0]
+         this.componentLoaded = true
       },
-      setCurrentCityInfo() {
+      async setCurrentCityInfo() {
          const success = async (position) => {
             const latitude = position.coords?.latitude;
             const longitude = position.coords?.longitude;
-            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=10ecbab3e9aca4de70736689048eeee4&units=metric`)
-            this.weatherInfo = response.data
-            this.weatherMainInfo = response.data.weather[0]
+            this.weatherInfo = await getWeatherInfo({ lat: latitude, lon: longitude })
+            this.weatherMainInfo = await this.weatherInfo.weather[0]
 
-            if (!this.cities.includes(response.data.name)) {
-               this.cities.push(response.data.name.toLowerCase())
+            if (!this.cities.includes(this.weatherInfo.name)) {
+               this.cities.push(this.weatherInfo.name.toLowerCase())
             }
             localStorage.setItem('cities', JSON.stringify(this.cities))
-            localStorage.setItem('chosenCity', JSON.stringify(response.data.name.toLowerCase()))
+            localStorage.setItem('chosenCity', JSON.stringify(this.weatherInfo.name.toLowerCase()))
          };
 
          const error = (err) => {
-            console.log(error)
+            console.log(err)
          };
          navigator.geolocation.getCurrentPosition(success, error);
+         this.componentLoaded = true
       },
       setSettings() {
          this.settings = this.settings ? false : true
@@ -119,18 +115,22 @@ export default {
       deleteCity(cityName) {
          this.cities = this.cities.filter(city => city !== cityName)
          localStorage.setItem('cities', JSON.stringify(this.cities))
-      }
+      },
    },
    mounted() {
       this.chosenCity = localStorage.getItem('chosenCity') ? JSON.parse(localStorage.getItem("chosenCity"))
          : this.setCurrentCityInfo()
-      this.fetchWeatherData()
-      this.setCities()
+      if (this.componentLoaded) {
+         this.fetchWeatherData()
+         this.setCities()
+      }
    },
    computed: {
       getUrl() {
-         return `http://openweathermap.org/img/wn/${this.weatherMainInfo.icon}@2x.png`
-      }
+         if (this.componentLoaded) {
+            return `http://openweathermap.org/img/wn/${this.weatherMainInfo.icon}@2x.png`
+         }
+      },
    }
 }
 </script>
@@ -145,6 +145,8 @@ export default {
    border: 1px solid black;
    background-color: cornsilk;
    border-radius: 15px;
+   display: flex;
+   justify-content: center;
 }
 
 .header-settings {
